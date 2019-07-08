a4158338a8273cff8ed237ba47722ef4fe328879
Rob Ladbrook <rob.ladbrook@deliveroo.co.uk>
2019-07-08 17:37:28 +0100

    Destroy

--- root.tf
+++ root.tf
@@ -3,8 +3,8 @@ provider "aws" {
   region     = "eu-west-1"
 }
 
-resource "aws_instance" "example" {
-  ami           = "ami-0bbc25e23a7640b9b"
-  instance_type = "t2.micro"
-  subnet_id = "subnet-f45003bd"
-}
+#resource "aws_instance" "example" {
+#  ami           = "ami-0bbc25e23a7640b9b"
+#  instance_type = "t2.micro"
+#  subnet_id = "subnet-f45003bd"
+#}
