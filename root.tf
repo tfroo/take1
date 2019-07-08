d07d45a23cfb39b122ce44306d44ca204e1ed593
Rob Ladbrook <rob.ladbrook@deliveroo.co.uk>
2019-07-08 16:25:29 +0100

    Switch region

--- root.tf
+++ root.tf
@@ -1,9 +1,9 @@
 provider "aws" {
   profile    = "default"
-  region     = "us-east-1"
+  region     = "eu-west-1"
 }
 
 resource "aws_instance" "example" {
-  ami           = "ami-2757f631"
+  ami           = "ami-0bbc25e23a7640b9b"
   instance_type = "t2.micro"
 }
